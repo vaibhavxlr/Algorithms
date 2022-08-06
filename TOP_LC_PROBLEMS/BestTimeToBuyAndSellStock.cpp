a35a60becf32bfab8162aae9da0edd39f7349bae
Vaibhav Upadhyay <vaibhavxlr@gmail.com>
2022-08-06 13:00:40 +0530

    commit msg

--- TOP_LC_PROBLEMS/BestTimeToBuyAndSellStock.cpp
+++ TOP_LC_PROBLEMS/BestTimeToBuyAndSellStock.cpp
@@ -5,3 +5,20 @@
  * @brief https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
  * 
  */
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProfit = 0;
+        int dayShareBought = 0;
+        
+        for(int i = 1; i < prices.size(); i++) {
+            maxProfit = max(maxProfit, prices[i] - prices[dayShareBought]);
+            if(prices[i] < prices[dayShareBought]) {
+                dayShareBought = i;
+            }
+        }
+        
+        return maxProfit;
+    }
+};
