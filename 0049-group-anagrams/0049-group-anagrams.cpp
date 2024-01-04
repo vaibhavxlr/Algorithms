cff0ea5c503b0b5903e4a2d072a62a9da13f9498
Vaibhav Upadhyay <vaibhavxlr@gmail.com>
2024-01-04 17:57:23 +0530

    Time: 36 ms (37.97%), Space: 24.6 MB (13.89%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,23 +1,38 @@
 class Solution {
+private:
+    void customSort(string& temp) {
+        vector<int> arr(26, 0);
+        int index = 0, j = 0;
+
+        for (int i = 0; i < temp.size(); i++) {
+            arr[temp[i] - 'a']++;
+        }
+
+        while (j < arr.size()) {
+            if (arr[j] == 0) {
+                j++;
+            } else {
+                temp[index++] = 'a' + j;
+                arr[j]--;
+            }
+        }
+    }
+
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> ans;
-        unordered_map<string, vector<int>> mp;
-        int index = 0;
-        
-        for(auto str : strs) {
-            sort(str.begin(), str.end());
-            mp[str].push_back(index++);
+        unordered_map<string, vector<string>> mp;
+
+        for (auto str : strs) {
+            string temp = str;
+            customSort(temp);
+            mp[temp].push_back(str);
         }
-        
-        for(auto items: mp) {
-            vector<string> temp;
-            for(auto ind : items.second) {
-                temp.push_back(strs[ind]);
-            }
-            ans.push_back(temp);
+
+        for (auto items : mp) {
+            ans.push_back(items.second);
         }
-        
+
         return ans;
     }
 };
