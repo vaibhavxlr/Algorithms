d5fa3c5b997753d57f94896f2ec508d606df57de
Vaibhav Upadhyay <vaibhavxlr@gmail.com>
2024-04-09 20:43:20 +0530

    Time: 5 ms (14.42%), Space: 9.2 MB (45.27%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
 
-    // void helper(int start, int end, vector<int>& nums, vector<vector<int>>& solution) {
-    //     if(start > end) {
-    //         return;
-    //     }
+    void helper(int start, int end, vector<int>& nums, vector<vector<int>>& solution) {
+        if(start > end) {
+            solution.push_back({});
+            return;
+        }
 
-
-    // }
+        helper(start + 1, end, nums, solution);
+        vector<vector<int>> temp;
+        for(auto it : solution) {
+            temp.push_back(it);
+            it.push_back(nums[start]);
+            temp.push_back(it);
+        }
+        solution.clear();
+        solution.assign(temp.begin(), temp.end());
+    }
 
     vector<vector<int>> subsets(vector<int>& nums) {
-        
-        if(nums.size() == 0) {
-            return {{}};
-        }
-
-        vector<int> newNums(nums.begin() + 1, nums.end());
-        vector<vector<int>> temp = subsets(newNums);
         vector<vector<int>> solution;
-
-        for(auto it : temp) {
-            solution.push_back(it);
-            vector<int> s3t(it.begin(), it.end());
-            s3t.push_back(nums[0]);
-            solution.push_back(s3t);
-        }
-
+        helper(0, nums.size() - 1, nums, solution);
         return solution;
     }
 };
