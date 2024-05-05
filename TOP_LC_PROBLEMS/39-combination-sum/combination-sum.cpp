9334e95f0a71891f36868329616a863db211c383
Vaibhav Upadhyay <vaibhavxlr@gmail.com>
2024-05-05 12:38:12 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (53.03%) - LeetSync

--- TOP_LC_PROBLEMS/39-combination-sum/combination-sum.cpp
+++ TOP_LC_PROBLEMS/39-combination-sum/combination-sum.cpp
@@ -1,29 +1,17 @@
 class Solution {
-private:
-    template<typename T>
-    struct VectorHash {
-        size_t operator()(const vector<T>& vec) const {
-            size_t hasher = 0;
-            for(auto& val : vec) {
-                hasher ^= hash<T>()(val) + 0x9e3779b9 + (hasher << 6) + (hasher >> 2);
-            }
-            return hasher;
-        }
-    };
+private:    
     vector<vector<int>> ans;
-    unordered_set<vector<int>, VectorHash<int>> s;
-    void solve(vector<int>& candidates, int target, vector<int> curr) {
+    
+    void solve(vector<int>& candidates, int start, int target, vector<int>& curr) {
         if (target <= 0) {
-            sort(curr.begin(), curr.end());
-            if (target == 0 && s.find(curr) == s.end()) {
+            if (target == 0) {
                 ans.push_back(curr);
-                s.insert(curr);
             }
             return;
         }
-        for (int i = 0; i < candidates.size(); i++) {
+        for (int i = start; i < candidates.size(); i++) {
             curr.push_back(candidates[i]);
-            solve(candidates, target - candidates[i], curr);
+            solve(candidates, i, target - candidates[i], curr);
             curr.pop_back();
         }
     }
@@ -31,7 +19,7 @@ private:
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<int> curr;
-        solve(candidates, target, curr);
+        solve(candidates, 0, target, curr);
         return ans;
     }
 };
